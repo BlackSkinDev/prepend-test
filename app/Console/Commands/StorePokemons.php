114b41541a98beefaf51e1a5bca57f2af28e0e16
Afeez Azeez <chillingloccini1@gmail.com>
2022-03-10 00:05:42 +0100

    added csv file containing pokemons, implemented storing of poekmons in DB in StorePokemons Command

--- app/Console/Commands/StorePokemons.php
+++ app/Console/Commands/StorePokemons.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Pokemon;
 use Illuminate\Console\Command;
 
 class StorePokemons extends Command
@@ -37,6 +38,44 @@ class StorePokemons extends Command
      */
     public function handle()
     {
-        return 0;
+        $file_path= storage_path('app/public/pokemon.csv');
+        $file = fopen($file_path,"r");
+
+        // go through file content and extract needed data into an array
+        $importData_arr = array();
+        $i = 0;
+        while (($filedata = fgetcsv($file, 1000, ",")) !== FALSE) {
+           $num = count($filedata );
+           if($i == 0){
+              $i++;
+              continue;
+           }
+           for ($c=0; $c < $num; $c++) {
+              $importData_arr[$i][] = $filedata [$c];
+           }
+           $i++;
+        }
+        fclose($file);
+
+        // Insert to MySQL database
+
+        $insertData = [];
+
+        // store all pokimons into single array
+        foreach($importData_arr as $importData){
+            $insertData []= [
+               "identifier"=>$importData[1],
+               "species_id"=>$importData[2],
+               "height"=>$importData[3],
+               "weight"=>$importData[4],
+               "base_experience"=>$importData[5],
+               "order"=>$importData[6],
+               'is_default'=>$importData[7],
+
+            ];
+          }
+        // save array of pokimons
+        Pokemon::insert($insertData);
+
     }
 }
