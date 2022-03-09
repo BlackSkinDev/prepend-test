0be4e044c2fd725526ff2b49ba633792c2f52c72
Afeez Azeez <chillingloccini1@gmail.com>
2022-03-09 23:40:38 +0100

    implemented exception handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,8 +1,14 @@
 <?php
-
 namespace App\Exceptions;
 
+use Exception;
+use Throwable;
+use Illuminate\Http\Response;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -26,12 +32,50 @@ class Handler extends ExceptionHandler
     ];
 
     /**
-     * Register the exception handling callbacks for the application.
+     * Report or log an exception.
      *
+     * @param  \Exception  $exception
      * @return void
+     *
+     * @throws \Exception
      */
-    public function register()
+    public function report(Throwable $exception)
     {
-        //
+        parent::report($exception);
     }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Exception
+     */
+    public function render($request,Throwable $e)
+    {
+        if($e instanceof  ModelNotFoundException || $e instanceof NotFoundHttpException){
+            return response()->json(['message'=>'Resource not found'],Response::HTTP_NOT_FOUND);
+        }
+
+        if($e instanceof  AuthenticationException ){
+            return response()->json(['message'=>'Unauthenticated'],Response::HTTP_UNAUTHORIZED);
+        }
+
+        if ($e instanceof UnauthorizedHttpException) {
+            return response()->json(['message'=>$e->getMessage()],Response::HTTP_UNAUTHORIZED);
+        }
+
+
+       return response()->json(['message'=>$e->getMessage()],Response::HTTP_INTERNAL_SERVER_ERROR);
+
+
+    }
+
+
+
+
 }
+
+
