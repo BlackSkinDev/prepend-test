487325f7bc154576f09cee4f61892efd79ad4c58
Afeez Azeez <chillingloccini1@gmail.com>
2022-03-09 23:37:43 +0100

    creating api resource route and Pokemon controller methods

---
+++ app/Http/Controllers/Api/PokemonController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PokemonController extends Controller
+{
+
+
+    // get all pokemons
+    public function index(){
+
+    }
+
+    // get single pokemon
+    public function show(){
+
+    }
+
+    // update single pokemon
+    public function update(){
+
+    }
+
+}
